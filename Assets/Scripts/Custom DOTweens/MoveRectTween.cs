ad3953c22240c7d0ad5152396cb1b44e57925a7e
KaynirGames <kaynir.games@gmail.com>
2021-04-30 21:46:36 +0300

    Залил SFX получения бонусов, заменил SFX выстрела из SMG, интегрировал DOTween, начал делать главное меню.

---
+++ Assets/Scripts/Custom DOTweens/MoveRectTween.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class MoveRectTween : MonoBehaviour
+{
+    [SerializeField] private RectTransform _targetRect = null;
+    [SerializeField] private RectTransform _rectInitialPoint = null;
+    [SerializeField] private float _moveDuration = .5f;
+    [SerializeField] private Ease _ease = Ease.Unset;
+
+    private void OnEnable()
+    {
+        MoveRect();
+    }
+
+    public void MoveRect()
+    {
+        _targetRect.DOAnchorPos(_rectInitialPoint.anchoredPosition, _moveDuration)
+                   .SetEase(_ease)
+                   .From();
+    }
+}
---
+++ Assets/Scripts/Custom DOTweens/ScaleLoopTween.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ScaleLoopTween : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform = null;
+    [SerializeField] private float _endScale = 1.2f;
+    [SerializeField] private float _scaleDuration = 1f;
+    [SerializeField] private LoopType _loopType = LoopType.Yoyo;
+    [SerializeField] private int _loopAmount = -1;
+
+    private void OnEnable()
+    {
+        HandleScaleLoop();
+    }
+
+    public void HandleScaleLoop()
+    {
+        _targetTransform.DOScale(_endScale, _scaleDuration)
+                        .SetLoops(_loopAmount, _loopType);
+    }
+}
